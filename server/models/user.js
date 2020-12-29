23988aa952ab8bd7c600302817cea1470fd323c7
Naveen <naveensreevalsan@gmail.com>
2020-12-29 13:04:33 +0530

    addUser

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,28 @@
 const users={};
 
-const addUser = (userData) => {
+const addUser = ({userName, socket_id, room_id, team_name, rank, profilePicture}) => {
     try {
-      if(userData.userName) {
+
+        if(!userName || 
+        !socket_id || 
+        !profilePicture || 
+        !rank ){
+            throw new Error("Give all parameters");
+        }
+        if(userName) {
            throw new Error("User already exists");
-      }
-      const newUser = { socket_id : userData.socket_id, room_id : userData.room_id  }
-      users[ userData.userName]  = newUser;
-      return newUser;
+        }
+      
+        const newUser = { 
+            socket_id : socket_id, 
+            room_id : room_id , 
+            team_name : team_name , 
+            rank : rank , 
+            userName : userName , 
+            profilePicture : profilePicture 
+        }
+        users[userName]  = newUser;
+        return newUser;
     } catch(err) {
      return err;
    }
