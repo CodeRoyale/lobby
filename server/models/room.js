264f2243716f2282b4a398e7a023be46546417a1
Naveen <naveensreevalsan@gmail.com>
2020-12-30 16:32:01 +0530

    room refactor base code

---
+++ server/models/room.js
@@ -0,0 +1,25 @@
+// this is my db for now
+rooms = {};
+
+// create room
+// joinRoom
+// removeUserFromRoom
+// createTeam
+// joinTeam
+// leaveTeam
+// closeRoom
+// banMember
+// addPrivateList
+// registerVotes
+// doVeto
+// startCompetition
+// atLeastPerTeam
+// codeSubmission
+// getRoomData
+
+const createRoom = (roomConfig , roomState , roomCompetition , roomTeams) =>{
+    if(!roomConfig.userName){
+        throw new Error("Username not specified.");
+    }
+    
+}
