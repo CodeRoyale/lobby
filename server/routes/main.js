d957b21b5340e79505b36dcd9bf03f9770f0e717
vuld0 <chiragsonubablani@karunya.edu.in>
2021-10-30 02:58:59 +0530

    check husky-2

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) =>
   res.send(`CodeRoyale Lobby Server is up and running`)
 );
 
-module.exports = router
+module.exports = router;
